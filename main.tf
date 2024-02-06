539138bc78aae32d83bfcf14d10edf8c90f2d43d
veerabose <veeraa.bose@gmail.com>
2024-02-06 20:58:08 +0530

    v2

--- main.tf
+++ main.tf
@@ -24,9 +24,9 @@ resource "azurerm_resource_group" "rg" {
 }
 
 module "securestorage" {
-      source  = "app.terraform.io/Ck-Terraform/securestorage/azurerm"
-      version = "1.0.0"
-      location             = azurerm_resource_group.rg.location
-      resource_group_name  = azurerm_resource_group.rg.name
-      storage_account_name = "veerastrg3728"
+  source               = "app.terraform.io/Ck-Terraform/securestorage/azurerm"
+  version              = "1.0.0"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_name = "veerastrg3728"
 }
