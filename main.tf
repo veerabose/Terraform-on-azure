8ba13529495128697adcd82d617e7df6b4c44290
veerabose <veeraa.bose@gmail.com>
2024-02-06 21:30:59 +0530

    v5

--- main.tf
+++ main.tf
@@ -36,5 +36,5 @@ module "securestorage" {
   version              = "1.0.0"
   location             = azurerm_resource_group.rg.location
   resource_group_name  = azurerm_resource_group.rg.name
-  storage_account_name = "veerastrg6789
+  storage_account_name = "veerastrg6789"
 }
